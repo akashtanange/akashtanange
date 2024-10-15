c9eda7369f63dc17c0cd6c73b55f58e6e0ce89dd
akashtanange <akashtanange7@gmail.com>
2024-10-16 00:33:58 +0530

    I am modified

--- src/DAY1/Simple.java
+++ src/DAY1/Simple.java
@@ -5,6 +5,8 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to java");
+		System.out.println("I am akash");
+		System.out.println("I am working in Github");
 
 	}
 
